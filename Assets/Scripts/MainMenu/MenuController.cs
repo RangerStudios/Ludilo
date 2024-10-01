19e18d06d498075e92d6f5b37c70c868d03ce143
JTShocks <jacobdreyer2002@gmail.com>
2024-09-30 21:29:01 -0400

    Created GameManager and MenuController
    Setting up scripts for the main menu and what is needed.
    GameManager is a singleton so can be pulled and referenced

---
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+    //Base script to handling shifting between menus while the game is paused
+
+    //Continue
+    //Options
+    //Main Menu
+    //Quit
+
+
+
+    void SwitchMenu()
+    {
+        //Disables the current menu
+        //Activates new menu
+        //Sets the focus target for new menu for UI control
+    }
+
+
+
+
+    
+}
