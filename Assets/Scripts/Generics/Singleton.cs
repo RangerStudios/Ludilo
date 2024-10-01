633f6551825c5534f5e25884c67e4e542cbaeafb
JTShocks <jacobdreyer2002@gmail.com>
2024-09-30 21:09:44 -0400

    Update Singleton.cs
    Generic singleton class for static references or systems we only need one of

--- Assets/Scripts/Generics/Singleton.cs
+++ Assets/Scripts/Generics/Singleton.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
+public class Singleton<T> : MonoBehaviour where T : Component
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public static T Instance {get; private set;}
+    private void Awake()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 }
