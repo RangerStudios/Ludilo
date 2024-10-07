250edf09f209c7140ca0399dab1a5a648b6d2138
JTShocks <jacobdreyer2002@gmail.com>
2024-10-06 22:41:05 -0400

    Fixed pause menu forever in pause

--- Assets/Scripts/MainMenu/PauseMenu.cs
+++ Assets/Scripts/MainMenu/PauseMenu.cs
@@ -64,6 +64,7 @@ public class PauseMenu : MonoBehaviour
    public void BackToMenu()
    {
      SceneManager.LoadScene("MainMenu");
+     GameManager.GameUnpause?.Invoke();
    }
 
    public void Quit()
