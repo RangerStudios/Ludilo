f1e91fd9db09ea1588154315b1d748298f7e98df
reireizo <nathan.irwin2021@gmail.com>
2024-10-17 17:04:03 -0400

    Cleanup and Proto Ambush Behavior
    Enemy Sight Service changed to Enemy Seen Service, sending a bool and not having a specified range. Behavior is now in-line with what is expected.

---
+++ Assets/Scripts/Pinhead.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pinhead : MonoBehaviour
+{
+    // (Prototype behavior) Will send a debug log message and disable behavior for a brief time.
+
+    public void ProtoGrab()
+    {
+        // Debug Message
+        // Disable behavior
+        // Timer for disable
+        // Re-enable behavior when timer ends
+        Debug.Log("Grabbed!");
+    }
+}
