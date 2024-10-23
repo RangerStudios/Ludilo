1d94d66867fe8aafb67ca1ffd9732976e9a2c0d7
reireizo <nathan.irwin2021@gmail.com>
2024-10-23 18:48:13 -0400

    Enemy-Side action complete

--- Assets/Scripts/Pinhead.cs
+++ Assets/Scripts/Pinhead.cs
@@ -1,17 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Pinhead : MonoBehaviour
+public class Pinhead : MonoBehaviour, IDamageable
 {
-    // (Prototype behavior) Will send a debug log message and disable behavior for a brief time.
+    public UnityEvent<int> onGrab;
+    public UnityEvent onRelease;
+    public HealthController EnemyHealth;
+    public GameObject behaviorObject;
+    public GameObject player;
+    public float grabTimer = 5f;
+    public float waitTimer = 3f;
+    private float currentTimer;
+    private bool hasGrabbed = false;
+    private bool hasReleased = false;
 
-    public void ProtoGrab()
+    void Update()
     {
-        // Debug Message
-        // Disable behavior
-        // Timer for disable
-        // Re-enable behavior when timer ends
+        if (hasGrabbed)
+        {
+            currentTimer -= Time.deltaTime;
+            if (currentTimer < 0)
+            {
+                onRelease.Invoke();
+            }
+        }
+
+        if (hasReleased)
+        {
+            currentTimer -= Time.deltaTime;
+            if (currentTimer < 0)
+            {
+                behaviorObject.SetActive(true);
+            }
+        }
+    }
+
+    public void Damage(int damageValue)
+    {
+        EnemyHealth.Damage(damageValue);
+    }
+
+    public void Die()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void GrabAction()
+    {
+        onGrab.Invoke(1);
+    }
+
+    public void Grab()
+    {
         Debug.Log("Grabbed!");
+        currentTimer = grabTimer;
+        hasGrabbed = true;
+        behaviorObject.SetActive(false);
+        player.GetComponent<PlayerController>().Damage(1);
+        this.gameObject.transform.SetParent(player.transform);
+    }
+
+    public void Release()
+    {
+        Debug.Log("Released!");
+        currentTimer = waitTimer;
+        hasGrabbed = false;
+        hasReleased = true;
+        this.gameObject.transform.SetParent(null);
     }
 }
