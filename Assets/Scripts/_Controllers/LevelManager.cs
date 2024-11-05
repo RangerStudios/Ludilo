91cb1d515f7dedc41706c2c7816a162775b9af46
reireizo <nathan.irwin2021@gmail.com>
2024-11-05 17:23:30 -0500

    LevelManager Progress

--- Assets/Scripts/_Controllers/LevelManager.cs
+++ Assets/Scripts/_Controllers/LevelManager.cs
@@ -1,33 +1,58 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public string lastEvent;
-    public string[] eventList;
-    [SerializeField]public HashSet<string> levelEvents = new HashSet<string>();
+
+    public List<string> publicEventList;
+    // Last activated event.
+    public string mostRecentEvent;
+    // List of events in the level - instarted into the levelEvents hashset at start.
+    public string[] levelEventArray;
+    // List of previously activated events in the level. Used for saving progress.
+    public string[] previousLevelEventArray;
+    // Hashset containing all level events.
+    public HashSet<string> levelEvents = new HashSet<string>();
+    // Hashset containing all previously activated events. Activated events from levelEvents get added here.
+    public HashSet<string> previousLevelEvents = new HashSet<string>();
+    // Hashset containing all remaining events.
+    public HashSet<string> remainingLevelEvents = new HashSet<string>();
 
     void Start()
     {
         CreateLevelHashSet();
     }
 
+    // Creates the hashset of strings for elevel events based on the string array eventList.
     public void CreateLevelHashSet()
     {
-        if (eventList.Length == 0)
+        if (publicEventList.Count == 0)
         {
-            Debug.Log("Empty Event List!");
+            Debug.LogError("Empty Event List!");
             return;
         }
         else
         {
-            foreach (string eventName in eventList)
+            foreach (string eventName in publicEventList)
             {
                 levelEvents.Add(eventName);
             }
         }
     }
+
+    public void TriggerLevelEvent(string levelEventToTrigger)
+    {
+        if(previousLevelEvents.Contains(levelEventToTrigger))
+        {
+            //Event has already happened
+            return;
+        }
+        
+    }
+
+
 }
