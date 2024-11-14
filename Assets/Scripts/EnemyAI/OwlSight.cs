70301a255f695389a39705bab41e7692f1ba5ff2
reireizo <nathan.irwin2021@gmail.com>
2024-11-14 17:11:15 -0500

    Main Owl Progress
    Main Owl is done. Sits in one place and looks at you. While it looks, timer ticks down. If timer hits zero, uses GameManager.PlayerFell to respawn you at previous checkpoint.

---
+++ Assets/Scripts/EnemyAI/OwlSight.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OwlSight : MonoBehaviour
+{
+    public static Action<Transform> OnSeePlayer;
+    public static Action OnNotSeePlayer;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            OnSeePlayer(other.transform);
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            OnNotSeePlayer();
+        }
+    }
+}
