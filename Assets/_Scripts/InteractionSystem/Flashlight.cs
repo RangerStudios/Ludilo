e016bf878aeff1ba3324b3e967f808307f754813
Jacob Dreyer <jacobdreyer2002@gmail.com>
2025-01-14 16:59:57 -0500

    basic prefab for Flashlight
    Not working quite yet, but have the basic fields for the functioning

---
+++ Assets/_Scripts/InteractionSystem/Flashlight.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MediumItemPickup, IActivatable
+{
+    public bool canBeActivated { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    [Header("Flashlight")]
+    [SerializeField]
+    Light theLight;
+
+
+    [Header("Sound Effects")]
+    [SerializeField] AudioClip clickOn;
+    [SerializeField] AudioClip clickOff;
+
+
+
+    public void OnEnable()
+    {
+
+        if ((theLight != null))
+        {
+            theLight = GetComponent<Light>();
+        }
+
+    }
+    public void Activate()
+    {
+        //Code goes into here to 
+        //Activate 
+        theLight.enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        //Handle what happens when flashlight turns off
+        //Disable the dynamic light
+        theLight.enabled = false;
+    }
+
+    
+}
