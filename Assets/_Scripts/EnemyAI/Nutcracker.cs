72136709fdc782a046548958e674549f7301ccd3
reireizo <nathan.irwin2021@gmail.com>
2025-02-06 16:42:16 -0500

    Movement and Enum States
    Adds nodes to the behavior tree, as well as updates the states to using enums.

--- Assets/_Scripts/EnemyAI/Nutcracker.cs
+++ Assets/_Scripts/EnemyAI/Nutcracker.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR;
 
 // "Weeping Angel" style enemy. Chases the player when not shined with the flashlight. Attacks when close enough.
 
@@ -11,9 +13,14 @@ public class Nutcracker : MonoBehaviour, IFlashable
     // Reference to attack object. Briefly enabled in order to attack player.
     public GameObject attackObject;
 
+    /* Old Bool States
     // Bools for states. One for while the enemy is attacking, one for while the enemy is in cooldown.
     [SerializeField] private bool isAttacking;
     [SerializeField] private bool hasAttacked;
+    */
+
+    // State enum that determines current state.
+    States currentState = States.Active;
 
     // Timer float that ticks up towards a specified float value.
     [SerializeField] private float currentTimer = 0f;
@@ -25,28 +32,34 @@ public class Nutcracker : MonoBehaviour, IFlashable
     // Ticks down a respective timer based on the current state boolean.
     void Update()
     {
-        if (isAttacking)
+        switch (currentState)
         {
-            currentTimer += Time.deltaTime;
-            if (currentTimer >= attackTimeLength)
-            {
-                EnterCooldown();
-            }
-        }
-        else if (hasAttacked)
-        {
-            currentTimer += Time.deltaTime;
-            if  (currentTimer >= attackCooldown)
-            {
-                ExitCooldown();
-            }
+            case States.Active:
+                break;
+            case States.Attacking:
+               currentTimer += Time.deltaTime;
+                if (currentTimer >= attackTimeLength)
+                {
+                    EnterCooldown();
+                }
+                break;
+            case States.Cooldown:
+                currentTimer += Time.deltaTime;
+                if  (currentTimer >= attackCooldown)
+                {
+                    ExitCooldown();
+                }
+                break;
+            default:
+                Debug.Log("You shouldn't see this. The enemy has no state!");
+                break;
         }
     }
 
     // Enemy is lit up by Flashlight. Stop its movements. Only works when enemy is not attacking or in cooldown.
     public void Flash()
     {
-        if (!isAttacking && !hasAttacked)
+        if (currentState == States.Active)
         {
             behaviorObject.SetActive(false);
         }
@@ -56,7 +69,7 @@ public class Nutcracker : MonoBehaviour, IFlashable
     public void UnFlash()
     {
         //behaviorObject.SetActive(true);
-        if (!isAttacking && !hasAttacked)
+        if (currentState == States.Active)
         {
             behaviorObject.SetActive(true);
         }
@@ -68,7 +81,7 @@ public class Nutcracker : MonoBehaviour, IFlashable
         behaviorObject.SetActive(false);
         attackObject.SetActive(true);
         ResetTimer();
-        isAttacking = true;
+        ChangeState(States.Attacking);
     }
 
     // Enemy has finished attacking. Stop attacking, enter cooldown.
@@ -76,19 +89,33 @@ public class Nutcracker : MonoBehaviour, IFlashable
     {
         attackObject.SetActive(false);
         ResetTimer();
-        isAttacking = false;
-        hasAttacked = true;
+        ChangeState(States.Cooldown);
     }
 
     // Enemy has finished cooldown. Resume normal action.
     public void ExitCooldown()
     {
-        hasAttacked = false;
+        ChangeState(States.Active);
+        behaviorObject.SetActive(true);
     }
 
     // Reset timer to zero.
     public void ResetTimer()
     {
         currentTimer = 0f;
+    }
+
+    // List of states the enemy can be in.
+    private enum States
+    {
+        Active,
+        Attacking,
+        Cooldown
+    };
+
+    // Changes enemy state to new one passed in.
+    private void ChangeState(States newState)
+    {
+        currentState = newState;
     }
 }
