39bb7346e012ae9867991f6dfe42347efcad939f
Anthony Caruso <89607598+Aetreas@users.noreply.github.com>
2025-02-06 16:55:22 -0500

    Update Interactor.cs

--- Assets/_Scripts/InteractionSystem/Interactor.cs
+++ Assets/_Scripts/InteractionSystem/Interactor.cs
@@ -10,6 +10,7 @@ public class Interactor : MonoBehaviour
     [SerializeField] private float interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask interactableMask;
     private PlayerController playerController;
+    //bool interacting;
 
     private readonly Collider[] colliders = new Collider[3];
     [SerializeField] public int numFound;
@@ -21,6 +22,7 @@ public class Interactor : MonoBehaviour
     void OnEnable()
     {
         PlayerInput.onInteract += Interaction;
+        //"InteractionPerformed" and "InteractionCancelled" methods may work for press and hold functionality.
     }
 
     void OnDisable()
